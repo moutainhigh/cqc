c893ed608ac796f1f97998cc817338f6e397b8fb
master_wlz@163.com <>
2020-01-23 21:38:16 +0800

    cqc后台

--- cqc-admin/src/main/java/com/cqc/admin/controller/HomeController.java
+++ cqc-admin/src/main/java/com/cqc/admin/controller/HomeController.java
@@ -1,5 +1,13 @@
 package com.cqc.admin.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.cqc.admin.dto.resp.HomeDataDto;
+import com.cqc.admin.service.OrderService;
+import com.cqc.admin.service.UserService;
+import com.cqc.common.api.Result;
+import com.cqc.model.Order;
+import com.cqc.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +22,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HomeController {
 
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private OrderService orderService;
+
+
     @GetMapping("")
     public String index() {
         return "hello welcome cqc !";
     }
+
+    @GetMapping("/homeData")
+    public Result<HomeDataDto> homeData() {
+
+        Integer count = userService.count(new QueryWrapper<User>().ne("status", -1));
+
+        Integer orderTotal = orderService.count();
+
+        HomeDataDto dataDto = new HomeDataDto();
+        dataDto.setUserTotal(count);
+        dataDto.setOrderTotal(orderTotal);
+
+        return Result.success(dataDto);
+    }
+
 
 }
---
+++ cqc-admin/src/main/java/com/cqc/admin/dto/resp/HomeDataDto.java
@@ -0,0 +1,13 @@
+package com.cqc.admin.dto.resp;
+
+import lombok.Data;
+
+@Data
+public class HomeDataDto {
+
+    private int orderTotal;
+
+    private int userTotal;
+    
+
+}
